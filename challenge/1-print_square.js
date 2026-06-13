5834304e613d8277e61f3e546769b91c0dabc6ee
root <root@ip-10-42-103-27.ec2.internal>
2026-06-13 11:35:22 +0000

    Fix missing newline in 1-print_square.js

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-
-function printSquare(size) {
-    if (isNaN(size) || size <= 0) {
-        return;
-    }
-
-    for (let i = 0; i < size; i++) {
-        console.log('#'.repeat(size));
-    }
+/*
+ * Prints a square of a given size using the # character
+ */
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.exit(1);
 }
 
-const size = parseInt(process.argv[2], 10);
-printSquare(size);
+var size = parseInt(process.argv[2]);
+
+for (var i = 0 ; i < size ; i++) {
+    for (var j = 0 ; j < size ; j++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
