4a75a9c3ecdc6ba1d3b9c208bd23540af841e5c4
root <root@ip-10-42-103-27.ec2.internal>
2026-06-13 11:34:27 +0000

    Fix FizzBuzz ordering bug for multiples of 15

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,41 +1,25 @@
 #!/usr/bin/python3
-""" FizzBuzz
-
-"""
-
 import sys
 
-
 def fizzbuzz(n):
-    """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-    - For multiples of three print "Fizz" instead of the number and for
-    multiples of five print "Buzz".
-    - For numbers which are multiples of both three and five print "FizzBuzz".
-    """
     if n < 1:
         return
 
-    tmp_result = []
-
+    tmp_list = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
+        # The 15 check must come first
+        if i % 3 == 0 and i % 5 == 0:
+            tmp_list.append("FizzBuzz")
+        elif i % 3 == 0:
+            tmp_list.append("Fizz")
+        elif i % 5 == 0:
+            tmp_list.append("Buzz")
         else:
-            tmp_result.append(str(i))
-
-    print(" ".join(tmp_result))
-
+            tmp_list.append(str(i))
+    print(" ".join(tmp_list))
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
     number = int(sys.argv[1])
